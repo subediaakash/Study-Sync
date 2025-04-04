b0692a76e2ce16ad7c089efcb95c9ab7918f1049
spartan-readytocode <7007966033abc@gmail.com>
2025-04-04 21:01:29 +0530

    feat : fixing websockets

--- backend/websocket/src/index.ts
+++ backend/websocket/src/index.ts
@@ -7,17 +7,52 @@ server.listen(3080, () => {
 });
 
 const wss = new WebSocketServer({ server });
+const connections = new Set<WebSocket>();
 
-const connections: Map<WebSocket, { id: string }> = new Map();
+const studyRooms = new Map<string, WebSocket[]>();
 
 wss.on("connection", (ws) => {
+  let userId = null;
+  let roomId = null;
+
   ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-    connections.forEach((_, client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    const data = JSON.parse(message.toString());
+    switch (data.type) {
+      case "join":
+        userId = data.userId;
+        roomId = data.roomId;
+
+        if (!studyRooms.has(roomId)) {
+          studyRooms.set(roomId, []);
+        }
+        const room = studyRooms.get(roomId);
+        if (room) {
+          room.push(ws);
+        }
+
+        broadcastToRoom(
+          roomId,
+          {
+            type: "user_joined",
+            userId: userId,
+            timestamp: Date.now(),
+          },
+          userId
+        );
+
+        const roomOfIndividual = studyRooms.get(roomId);
+        const roomParticipants = roomOfIndividual
+          ? Array.from(roomOfIndividual)
+          : [];
+        ws.send(
+          JSON.stringify({
+            type: "room_state",
+            participants: roomParticipants,
+            timestamp: Date.now(),
+          })
+        );
+        break;
+    }
   });
   ws.on("close", () => {
     console.log("Client disconnected");
